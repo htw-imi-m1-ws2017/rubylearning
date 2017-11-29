dfc6d4c7b6997e2fa3e64500b52149831ddec9dd
tropicale1ddlh <Cantyouseesometimesyourwords>
2017-11-29 17:32:58 +0100

    done with the test.

--- about/map.rb
+++ about/map.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 describe 'map on enumerations' do
+  let(:an_map) { [ "a", "b", "c", "d" ] }
+
+  it '- adding elements' do
+    an_map.collect { |x| x + "!" }        #=> ["a!", "b!", "c!", "d!"]
+  end
+
+  it 'show element' do
+    expect(an_map[1]).to eq('b!')
+  end
+
 end
