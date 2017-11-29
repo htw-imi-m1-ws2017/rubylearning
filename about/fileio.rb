7ec3824244fefd2861153b39e38c994a0a190d56
s0562668 <s0562668@htw-berlin.de>
2017-11-29 11:26:25 +0100

    Add test for line equality

--- about/fileio.rb
+++ about/fileio.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
 describe 'File IO' do
-  # put files in temp
+  # create dir and file, write to it
   Dir.mkdir "temp"
   fname = "temp/test.txt"
   file = File.open(fname, "w")
-  file.puts "This is a file!"
+  lines = ["This is line 1!", "This is line 2!", "This is line 3!"]
+  file.puts lines
   file.close
 
+  # tests
+  i = 1
+  File.open(fname, "r") do |f|
+    f.each_line do |line|
+      it "Lines should equal" do
+        expect(line).to eq("This is line #{i}!\n")
+        i+=1
+      end
+    end
+  end
+
+  # delete file and dir
   File.delete(fname)
   Dir.delete("temp")
 end
