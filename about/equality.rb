f047705a1a0aca1233435b1650cae51723f8f2c4
WendtOle <dielanie@freenet.de>
2017-11-29 11:06:18 +0100

    add tests for equality of equal?

--- about/equality.rb
+++ about/equality.rb
@@ -2,7 +2,17 @@
 
 describe 'Ruby Comparision Operators' do
   context '== and equal? work the other way around compared to java!' do
-    it 'equal? checks for object identity'
-    it '== checks for equal value'
+    it 'equal? checks for object identity' do
+      class AObj
+      end
+      class BObj
+      end
+      expect(AObj == BObj).to eq(false)
+      expect(AObj.equal?BObj).to eq(false)
+      expect(AObj.equal?AObj).to eq(true)
+    end
+    it '== checks for equal value' do
+      expect(1 == 1).to eq(true)
+    end
   end
 end
